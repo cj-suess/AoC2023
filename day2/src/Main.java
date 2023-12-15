5939854f2f0764ecc0b7b5daad4f249844466cdc
cj-cweezy <camsuess@gmail.com>
2023-12-14 17:29:52 -0700

    finished part 1

--- day2/src/Main.java
+++ day2/src/Main.java
@@ -3,7 +3,7 @@ import java.io.*;
 import java.util.regex.*;
 public class Main {
     public static void main(String[] args) throws IOException {
-        File file = new File("day2/src/test.txt");
+        File file = new File("day2/src/data.txt");
         FileReader fr = new FileReader(file);
         BufferedReader br = new BufferedReader(fr);
         String line = br.readLine();
@@ -15,6 +15,48 @@ public class Main {
         ArrayList<Integer> possibleGames = new ArrayList<>();
 
         while(line != null){
+
+                int foundMatches = 0;
+                int counter = 0;
+                int gameNumber = 0;
+
+                String gameNumberRegex = "(\\d+)(?=:)";
+                Pattern gameNumberPattern = Pattern.compile(gameNumberRegex);
+                Matcher gameNumberMatcher = gameNumberPattern.matcher(line);
+
+                String colorNumRegex = "(\\d+)(\\s+)(\\w+)";
+                Pattern pattern = Pattern.compile(colorNumRegex);
+                Matcher matcher = pattern.matcher(line);
+
+                if(gameNumberMatcher.find()) {
+                    gameNumber = Integer.parseInt(gameNumberMatcher.group(1));
+                }
+
+                while(matcher.find()) {
+                    int num = Integer.parseInt(matcher.group(1));
+                    String color = matcher.group(3);
+
+                    if(colors.get(color) >= num){
+                        counter++;
+                    }
+
+                    foundMatches++;
+                }
+
+                if(counter == foundMatches){
+                    possibleGames.add(gameNumber);
+                }
+
+            line = br.readLine();
         }
+
+        System.out.println(possibleGames);
+
+        //sum possible games
+        int sum = 0;
+        for(int i = 0; i < possibleGames.size(); i++){
+            sum += possibleGames.get(i);
+        }
+        System.out.println(sum);
     }
 }
