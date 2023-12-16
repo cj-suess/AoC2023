69b6252d6add4ea6983bf60d2af1e6f4acb8e077
cj-cweezy <camsuess@gmail.com>
2023-12-16 09:02:38 -0700

    added python file for day3 part1

--- day3/src/part1.java
+++ day3/src/part1.java
@@ -6,11 +6,12 @@ import java.util.regex.*;
 
 public class part1 {
 
+
     public static void main(String[] args) throws IOException {
         File file = new File("day3/src/data.txt");
         FileReader fr = new FileReader(file);
         BufferedReader br = new BufferedReader(fr);
 
-
+        
     }
 }
