6758f1614a5b7fd4ad4ac3b1fcff9e096fdca2ef
cj-cweezy <camsuess@gmail.com>
2023-12-17 09:34:57 -0700

    starting day4

---
+++ day4/src/part1.java
@@ -0,0 +1,17 @@
+package AoC2023.day4.src;
+
+import java.util.*;
+import java.io.*;
+
+public class part1 {
+    public static void main(String[] args) throws IOException {
+
+        File file = new File("day4/src/data.txt");
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
+        String line = br.readLine();
+
+
+
+    }
+}
