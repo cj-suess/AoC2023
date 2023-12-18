82383bf0a054d41948f68ce4cfc9a2060349aeb0
cj-cweezy <camsuess@gmail.com>
2023-12-18 09:09:08 -0700

    finished day4 starting day5

--- day5/src/part1.java
+++ day5/src/part1.java
@@ -1,9 +1,3 @@
-package AoC2023.day5.src;
-
-import java.io.*;
-import java.util.*;
-import java.util.regex.*;
-
 public class part1 {
     public static void main(String[] args) {
         System.out.println("Hello world!");
