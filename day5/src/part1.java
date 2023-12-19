95c65c188cc1eeeff508a4641492d621e34b41b5
cj-cweezy <camsuess@gmail.com>
2023-12-19 10:54:27 -0700

    finished day5 part1, starting part2

--- day5/src/part1.java
+++ day5/src/part1.java
@@ -5,83 +5,92 @@ import java.util.*;
 import java.util.regex.*;
 import java.util.Map;
 
-public class part1 {
+//My first solution was brute force and did not work for the actual data set
 
-    public static String readFile(File file) throws IOException {
+//This plan inspired by a python solution, had to improvise for the for/else loop using for/if with a boolean
+
+//for each arraylist, create a ranges list
+    //for each line in the arraylist, add each line of nums to ranges list
+    //create a new list
+    //for each seed in seeds list
+        //for each num in ranges list
+            //if b <= seed < (b+c)
+                //append seed - b + a
+                //break
+        //else
+            //append seed to new
+
+    //seed = new (which will now be locations)
+
+//print min of seed (locations)
+
+public class part1 {
+    public static void main(String[] args) throws IOException {
+        ArrayList<Long> seeds = new ArrayList<>();
+        ArrayList<ArrayList<ArrayList<Long>>> maps = new ArrayList<>();
+        ArrayList<ArrayList<Long>> currentBlock = new ArrayList<>();
+
+        File file = new File("day5/src/data.txt");
         BufferedReader br = new BufferedReader(new FileReader(file));
         String line = br.readLine();
-        return line;
-    }
 
-
-    public static ArrayList<ArrayList<Integer>> getNums(File file) throws IOException {
-        ArrayList<ArrayList<Integer>> nums = new ArrayList<>();
-        BufferedReader br = new BufferedReader(new FileReader(file));
-        String line;
-
-        while((line = br.readLine()) != null){
-            Pattern p = Pattern.compile("\\d+");
-            Matcher m = p.matcher(line);
-            ArrayList<Integer> temp = new ArrayList<>();
-
-            while(m.find()){
-                temp.add(Integer.parseInt(m.group()));
+        while (line != null) {
+            if (line.contains("seeds:")) {
+                line = br.readLine();
+                if (line != null) {
+                    String[] nums = line.split(" ");
+                    for (String num : nums) {
+                        seeds.add(Long.parseLong(num));
+                    }
+                }
+            } else if (!line.matches(".*\\d+.*")) {
+                if (!currentBlock.isEmpty()) {
+                    maps.add(currentBlock);
+                    currentBlock = new ArrayList<>();
+                }
+            } else {
+                ArrayList<Long> lineData = new ArrayList<>();
+                String[] nums = line.split(" ");
+                for (String num : nums) {
+                    lineData.add(Long.parseLong(num));
+                }
+                currentBlock.add(lineData);
             }
-            if (!temp.isEmpty()) {
-                nums.add(temp);
-            }
+            line = br.readLine();
+        }
+
+        if (!currentBlock.isEmpty()) {
+            maps.add(currentBlock);
         }
         br.close();
-        return nums;
-    }
 
-    public static Map<Integer, Integer> buildMap(ArrayList<ArrayList<Integer>> nums) {
-        Map<Integer, Integer> map = new LinkedHashMap<>();
+        for (ArrayList<ArrayList<Long>> map : maps) {
 
-        // Initial mapping from 0 up to the first number in the first sequence
-        int firstNumber = nums.get(0).get(0);
-        for (int i = 0; i < firstNumber; i++) {
-            map.put(i, i);
-        }
-
-        // Process each sequence in nums
-        for (int s = 0; s < nums.size(); s++) {
-            ArrayList<Integer> sequence = nums.get(s);
-            if (sequence.size() < 3) continue; // Skip if the sequence does not have at least three elements
-
-            int start = sequence.get(0);
-            int end = sequence.get(1);
-            int value = sequence.get(2);
-
-            // Calculate how many times the mapping should increment
-            int incrementCount = end - start + 1;
-
-            for (int i = 0; i < incrementCount; i++) {
-                map.put(start + i, value + i);
+            ArrayList<ArrayList<Long>> ranges = new ArrayList<>();
+            for(ArrayList<Long> l : map){
+                ranges.add(l);
             }
+
+            ArrayList<Long> newSeeds = new ArrayList<>();
+            for(Long seed : seeds){
+                boolean found = false;
+                for(ArrayList<Long> range : ranges){
+                    long a = range.get(0);
+                    long b = range.get(1);
+                    long c = range.get(2);
+                    if((b <= seed) && (seed < (b+c))) {
+                        newSeeds.add((seed - b) + a);
+                        found = true;
+                        break;
+                    }
+                }
+                if(!found){
+                    newSeeds.add(seed);
+                }
+            }
+            seeds = newSeeds;
         }
 
-        return map;
-    }
-
-
-
-
-
-    public static void main(String[] args) throws IOException {
-
-        ArrayList<Integer> seedList = new ArrayList<>();
-        Map<Integer,Integer> seedToSoil = new LinkedHashMap<>();
-        Map<Integer,Integer> soilToFertilizer = new LinkedHashMap<>();
-        Map<Integer,Integer> fertilizerToWater = new LinkedHashMap<>();
-        Map<Integer,Integer> waterToLight = new LinkedHashMap<>();
-        Map<Integer,Integer> lightToTemp = new LinkedHashMap<>();
-        Map<Integer,Integer> tempToHumidity = new LinkedHashMap<>();
-        Map<Integer,Integer> humidityToLocation = new LinkedHashMap<>();
-
-        ArrayList<ArrayList<Integer>> temp = getNums(new File("day5/src/tests/seedToSoilTest.txt"));
-        seedToSoil = buildMap(temp);
-        System.out.println(seedToSoil);
-
+        System.out.println(Collections.min(seeds));
     }
 }
